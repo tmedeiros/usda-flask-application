97428cf49ded91e8de447e7927fa7f426108d162
idaf_ai <theo@idaf.ng>
2020-09-23 15:10:36 -0500

    removed environmental variables

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-
-
 @app.route('/')
 def hello():
     return "Hello World!"
