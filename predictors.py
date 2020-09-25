6cd1dd70eeaa1d88b42b87be0910e670b9218ad7
idaf_ai <theo@idaf.ng>
2020-09-25 03:34:36 -0500

    removed flask_bootstrap

--- predictors.py
+++ predictors.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
-from flask_bootstrap import Bootstrap
 
 
 class UserEntryForm(FlaskForm):
